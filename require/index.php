0562a8885e4ab3f59db65aa489f0053bbf0ae1dd
Molchanova Tatyana <mtuspeh@gmail.com>
2019-12-17 22:01:51 +0200

    n commit

---
+++ require/index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <p>Lorem ipsum dolor sit amet.</p>
+
+    
+
+
+    <?php include_once 'inc/inc.php' ?>
+    <?php include_once 'inc/inc.php' ?>
+
+    <p>Lorem ipsum dolor sit amet.</p>
+
+</body>
+</html>
